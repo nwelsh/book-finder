8a4d8f5585c69c22a34b8d3c1fdfd18f3d1ebb3a
nwelsh <newelsh@wisc.edu>
2026-05-28 09:53:45 -0500

    next config

--- book-finder/next.config.ts
+++ book-finder/next.config.ts
@@ -1,20 +1,10 @@
+import type { NextConfig } from "next";
 
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-=======
 const nextConfig: NextConfig = {
->>>>>>> f0d4ca4 (deploy)
   output: "export",
   images: {
     unoptimized: true,
   },
 };
 
-<<<<<<< HEAD
-module.exports = nextConfig;
-
 export default nextConfig;
-=======
-export default nextConfig;
->>>>>>> f0d4ca4 (deploy)
